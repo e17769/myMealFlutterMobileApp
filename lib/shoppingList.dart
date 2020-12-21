284fdadf43c12571b4ec34306aa56be243e7f680
e17769 <E17769@hotmail.com>
2020-12-20 23:38:03 -0500

    update the shopping list background image

--- lib/shoppingList.dart
+++ lib/shoppingList.dart
@@ -4,20 +4,50 @@ import 'package:flutter/src/widgets/framework.dart';
 
 //Shopping list page
 class ShoppingList extends StatelessWidget {
+  void navigateToHomePage(BuildContext context) {
+    Navigator.pop(context);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Second Route"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          'images/agbg.jpg',
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          fit: BoxFit.cover,
         ),
-      ),
+        Scaffold(
+          appBar: AppBar(
+            title: const Text('Shooping List'),
+          ),
+          body: Container(
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('images/shopping.jpg'),
+                    fit: BoxFit.cover)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                const Text(
+                  'Shopping list comming soon',
+                  style: TextStyle(color: Colors.white, fontSize: 30),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            //onPressed: _reset,
+            onPressed: () => navigateToHomePage(context),
+            tooltip: 'Reset',
+            child: const Icon(Icons.home),
+          ),
+          // This trailing comma makes auto-formatting nicer for build methods.
+        )
+      ],
     );
   }
 }
